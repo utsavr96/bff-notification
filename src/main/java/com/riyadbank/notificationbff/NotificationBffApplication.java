1990d1406fe6de207458142039ed61a4a586bd1d
Utsav <utsavrastogi@outlook.com>
2024-03-17 21:04:46 +0530

    first commit

---
+++ src/main/java/com/riyadbank/notificationbff/NotificationBffApplication.java
@@ -0,0 +1,13 @@
+package com.riyadbank.notificationbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationBffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/riyadbank/notificationbff/NotificationBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riyadbank.notificationbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
